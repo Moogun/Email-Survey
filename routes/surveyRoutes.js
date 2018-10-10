cf02e03cd79cff157405e3cc24994994dd04f9f0
moogun <moogunjung@gmail.com>
2018-10-10 11:34:16 +0900

    installed sendgrid and added sendgrid key

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,5 +3,18 @@ const requireCredits = require('../middlewares/requireCredits')
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+    const {title, subject, body, recipients} = req.body
+    console.log(title, subject, body, recipients);
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({email: email})),
+      _user: req.user.id,
+      dateSent: Date.now()
+    })
+
+
   })
 }
