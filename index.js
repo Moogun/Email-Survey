f7f8f1e7c692619d4c3c15ce75fce9a1e8d05bde
moogun <moogunjung@gmail.com>
2018-10-10 09:43:07 +0900

    added requireLogin middleware to billingRoutes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('on port or 3000');
+  console.log(`on port ${PORT}`);
 });
