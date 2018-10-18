2de7e88bad224cdadee6b473831554fbaacf9471
moogun <moogunjung@gmail.com>
2018-10-18 22:08:30 +0900

    installed react-loadable, and added loadableSurveyList

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,16 @@ import * as actions from './actions'
 import Header from './components/Header'
 import Landing from './components/Landing'
 import SurveyNew from './components/surveys/SurveyNew'
-import SurveyList from './components/surveys/SurveyList'
 import DraftList from './components/surveys/DraftList'
 import './App.css'
+
+import Loadable from 'react-loadable'
+const loadableSurveyList = Loadable({
+    loader: () => import('./components/surveys/SurveyList'),
+    loading() {
+        return  <div>loading</div>
+    }
+})
 
 class App extends Component {
 
@@ -23,7 +30,7 @@ class App extends Component {
           <div>
             <Header />
             <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={SurveyList} />
+            <Route exact path="/surveys" component={loadableSurveyList} />
             <Route path="/surveys/new" component={SurveyNew} />
             <Route exact path="/draft" component={DraftList} />
           </div>
