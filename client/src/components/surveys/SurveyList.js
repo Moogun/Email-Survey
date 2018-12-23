2175458f03250837978e4676714644f11a8fb46e
Moo-Gun Jung <moogunjung@gmail.com>
2018-12-23 22:47:18 +0900

    replaced semantic ui table with custom table

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { fetchSurveys } from '../../actions'
 
-import Container from 'semantic-ui-react/dist/commonjs/elements/Container';
 import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
-import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import Pagination from 'semantic-ui-react/dist/commonjs/addons/Pagination';
-import Table from 'semantic-ui-react/dist/commonjs/collections/Table';
 
 class SurveyList extends Component {
 
@@ -33,14 +30,17 @@ class SurveyList extends Component {
           return surveys.page.map(survey => {
             let date = new Date(survey.dateSent);
             return (
-                <Table.Row key={survey._id}>
-                  <Table.Cell>{date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate()} </Table.Cell>
-                  <Table.Cell>{survey.title} </Table.Cell>
-                  <Table.Cell>{survey.body} </Table.Cell>
-                  <Table.Cell>{survey.recipients.length}</Table.Cell>
-                  <Table.Cell>{survey.yes} </Table.Cell>
-                  <Table.Cell>{survey.no} </Table.Cell>
-                </Table.Row>
+                  <tr key={survey._id}>
+                    <td>{date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate()}</td>
+                    <td>{survey.title}</td>
+                    <td>{survey.body}</td>
+                    <td>{survey.recipients.length}</td>
+                    <td>Delivered</td>
+                    <td>open</td>
+                    <td>Unique Open</td>
+                    <td>Click</td>
+                    <td>{survey.yes} : {survey.no}</td>
+                  </tr>
             )
           })
       }
@@ -52,40 +52,45 @@ class SurveyList extends Component {
     // console.log('[render surveys]', this.props.surveys, chosenPage);
 
     return (
-      <Container>
-        <div className="section">
-          <h1 className="text-box">
-            <span className="section-heading">Survey List</span>
+      <>
+        <div class="overview">
+          <h1 class="overview__heading">
+            Dashboard
           </h1>
         </div>
-        <Table padded selectable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell width={1}>Date Sent</Table.HeaderCell>
-              <Table.HeaderCell width={2}>Title</Table.HeaderCell>
-              <Table.HeaderCell width={3}>Body</Table.HeaderCell>
-              <Table.HeaderCell width={1}>Num of Recipients</Table.HeaderCell>
-              <Table.HeaderCell width={1}>Reply: Yes</Table.HeaderCell>
-              <Table.HeaderCell width={1}>Reply: No</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-              {this.renderSurveys()}
-          </Table.Body>
-         </Table>
+        <div class="detail">
+          <div class="description">
+            <div className="campaign__list">
+               <table>
+                   <tr>
+                     <th>Date</th>
+                     <th>Campaign</th>
+                     <th>Sent</th>
+                     <th>Delivered</th>
+                     <th>Open</th>
+                     <th>Unique Open</th>
+                     <th>Click</th>
+                     <th>Unique Click</th>
+                     <th>sth</th>
+                   </tr>
+                   {this.renderSurveys()}
+                 </table>
+             </div>
 
-        <Segment basic textAlign='center'>
-            <Pagination
-                activePage={!!chosenPage ? chosenPage : 0}
-                boundaryRange="1"
-                onPageChange={this.handlePaginationChange}
-                size='mini'
-                siblingRange="1"
-                totalPages={!!count ? parseInt(count / 5, 10) + 1 : 0}
-                secondary
-             />
-         </Segment>
-      </Container>
+           <Segment basic textAlign='center'>
+               <Pagination
+                   activePage={!!chosenPage ? chosenPage : 0}
+                   boundaryRange="1"
+                   onPageChange={this.handlePaginationChange}
+                   size='mini'
+                   siblingRange="1"
+                   totalPages={!!count ? parseInt(count / 5, 10) + 1 : 0}
+                   secondary
+                />
+            </Segment>
+          </div>
+        </div>
+      </>
     );
   }
 
