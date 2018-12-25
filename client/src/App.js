dd45806e86ac6a84d49b47fcd3899b9b596fb309
Moo-Gun Jung <moogunjung@gmail.com>
2018-12-25 17:28:44 +0900

    fixed wrong changes made from previous commit

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
           return <Landing />
       default :
           return <div className="content">
-            <nav className="sideDrawer">
+            <nav className="sidebar">
               <ul className="side-nav">
                 {/* side-nav__item--active */}
                 <li className="side-nav__item">
